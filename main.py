26798b56078892e0b32be966fe3038b059844002
Fatima umer <fatimaumerbhalli@gmail.com>
2026-03-22 00:03:27 +0500

    Initialize commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
 
-#1st function
-def fun1(n):
-    return math.factorial(n)
-print(fun1(5))
+
+#2nd function
+def fun2(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+        return result
+print(fun2(5))
